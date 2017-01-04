01ef53ce96f9283fc763098bd664d4600f7debe6
c00z <zcusimano@gmail.com>
2017-01-04 14:38:09 -0800

    Finished challenge 2, exercise 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -2,6 +2,8 @@ console.log('challenge #2 -- app.js loaded!');
 
 var app = angular.module("appTwo", []);
 app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
+
 
 function WelcomeController(){
   this.full_name = "Zach Cusimano";
@@ -12,3 +14,24 @@ function WelcomeController(){
     return yell.toUpperCase() + "!";
   }
 }
+
+function ClassController() {
+  this.class_name = "WDI 34";
+  this.enrolled_students = "11";
+  this.start_date = "11/28/2016"
+  this.end_date = "3/03/2017"
+  this.daysRemaining = function() {
+    var ms_left = Date.parse(this.end_date) - Date.now();
+    var days_left = Math.floor( ms_left/1000/24/60/60 );
+    return days_left;
+  }
+  }
+
+
+//
+// li>Class Name: {{ "class_name" }}</li>
+// <li>Students: {{ "enrolled_students" }}</li>
+// <li>Start: {{ "start_date" }}</li>
+// <li>End: {{ "end_date" }}</li>
+// <!-- HINT: Create a daysRemaining() function in your controller that calculate this -->
+// <li>Days Left: {{ "daysRemaining()" }}</li>
