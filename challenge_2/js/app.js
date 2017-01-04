0b03de07b4ff026974243a019511a8ae6293a6bb
c00z <zcusimano@gmail.com>
2017-01-04 14:22:44 -0800

    Finished challenge two, exercise 1

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,14 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
-//
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME";
-// }
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+
+function WelcomeController(){
+  this.full_name = "Zach Cusimano";
+  this.age = "28";
+  this.city = "Las Vegas";
+  this.state = "Nevada";
+  this.shout = function(yell){
+    return yell.toUpperCase() + "!";
+  }
+}
